9756a9eedf3da45faf46b485e46c758d8936a6bf
Arturo <101345517+Melinnaaa@users.noreply.github.com>
2024-05-26 21:57:39 -0400

    si

---
+++ Proyecto/entrega2/control.cpp
@@ -0,0 +1,65 @@
+#include <NewPing.h>
+
+#define TRIGGER_PIN  12
+#define ECHO_PIN     11
+#define MAX_DISTANCE 400
+
+#define MOTOR_RIGHT_PWM 5
+#define MOTOR_RIGHT_DIR 6
+#define MOTOR_LEFT_PWM  9
+#define MOTOR_LEFT_DIR  10
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+unsigned int distance = 0;
+
+double setpoint = 30.0; // Setpoint en centímetros
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(MOTOR_RIGHT_PWM, OUTPUT);
+  pinMode(MOTOR_RIGHT_DIR, OUTPUT);
+  pinMode(MOTOR_LEFT_PWM, OUTPUT);
+  pinMode(MOTOR_LEFT_DIR, OUTPUT);
+}
+
+void loop() {
+  distance = sonar.ping_cm();
+  
+  // Envía la distancia medida a través del puerto serial
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  
+  // Recibe el valor del error y el PID desde Python
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    int commaIndex = input.indexOf(',');
+    double error = input.substring(0, commaIndex).toDouble();
+    double output = input.substring(commaIndex + 1).toDouble();
+
+    
+    // Ajuste de umbrales basado en valores típicos del PID
+    int motorSpeed;
+    if (abs(output) > 12) { // Valor alto de PID, robot muy lejos
+      motorSpeed = 255; // Velocidad máxima
+    } else if (abs(output) < 9 && abs(output) > 5) { // Valor mediano de PID
+      motorSpeed = 128; // Velocidad media
+    } else if (abs(output) <= 5) { // Valor bajo de PID
+      motorSpeed = 40; // Velocidad baja
+    }
+
+    // Ajusta la dirección basada en la salida del PID.
+    if (output < 0) {
+      digitalWrite(MOTOR_RIGHT_DIR, HIGH);
+      digitalWrite(MOTOR_LEFT_DIR, HIGH);
+    } else {
+      digitalWrite(MOTOR_RIGHT_DIR, LOW);
+      digitalWrite(MOTOR_LEFT_DIR, LOW);
+    }
+
+    analogWrite(MOTOR_RIGHT_PWM, motorSpeed);
+    analogWrite(MOTOR_LEFT_PWM, motorSpeed);
+    
+  }
+
+  delay(50);
+}
